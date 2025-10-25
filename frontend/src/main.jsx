5e5351871f058192f251f7aa820b7f0006a3f17c
saminajib <saminajib2000@gmail.com>
2025-10-25 15:57:24 -0400

    updated @

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import GraphTest from './pages/GraphTest'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard />
+    <GraphTest />
   </StrictMode>,
 )
