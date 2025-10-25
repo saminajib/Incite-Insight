68594830c516e0f80ef8569b025b6c20f6e86fea
crouse_luke <lukecrouse264@gmail.com>
2025-10-25 13:44:53 -0400

    implemented gemini api with small test

--- server/server.js
+++ server/server.js
@@ -2,10 +2,20 @@ import express from "express";
 import {parse} from "csv-parse";
 import multer from "multer";
 import fs from "fs";
+import { GoogleGenAI } from "@google/genai";
+import { config } from "dotenv";
+import path from "path";
+config({ path: path.resolve(process.cwd(), "server/.env") });
+
 
 const app = express();
 const port = 3000;
 const upload = multer({dest:'uploads/'});
+
+
+const ai = new GoogleGenAI({});
+
+
 
 const parseCSV = (filePath) => {
     return new Promise((resolve, reject) => {
@@ -33,7 +43,25 @@ app.post("/upload", upload.single('file'), async (req, res) => {
   }
 });
 
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
+
+
+
+async function testGemini() {
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+(async () => {
+    try {
+      await testGemini();
+    } catch (err) {
+      console.error("Gemini test failed:", err);
+    }
+  })();
+  
